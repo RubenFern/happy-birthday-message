6509676c5f43eb5bd77947c60d8a772962d77025
RubenFern <rubenferval@hotmail.com>
2024-03-31 12:15:33 +0200

    changes

--- webapp/src/app/ballons/ballons.component.ts
+++ webapp/src/app/ballons/ballons.component.ts
@@ -21,13 +21,18 @@ export class BallonsComponent implements OnInit
     ngOnInit(): void
     {
         let numBallons = 0;
+        console.log(window.innerWidth);
 
         if (window.innerWidth < 400)
-            numBallons = 7;
+            numBallons = 4;
         else if (window.innerWidth < 800)
-            numBallons = 12;
+            numBallons = 8;
+        else if (window.innerHeight < 1500)
+            numBallons = 19;
         else
             numBallons = 27;
+
+        console.log(numBallons);
 
         this.numBallonsArray = Array(numBallons).fill(0).map((x, i) => i);
     }
